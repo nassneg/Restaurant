ef663ee5413c8aba1881c53c7927782cd9aac228
nassneg <snailfail@yandex.ru>
2021-09-10 21:18:20 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+const slider = document.querySelector(".reviews__slider")
+const carousel = document.querySelector(".reviews__carousel")
+const dots = document.querySelectorAll(".reviews__dot")
+
+const maxSlides = 3
+const positionsDesktop = [0, 600, 1200]
+const positionsTablet = [0, 400, 800]
+const positionsMobile = [0, 250, 500]
+currentSlide = 0
+
+const selectSlide = (index) => {
+    if (window.screen.width >= 640) {
+        carousel.style.transform = `translateX(-${positionsDesktop[index]}px)`
+        currentSlide = index
+      } else if (window.screen.width >= 440) {
+        carousel.style.transform = `translateX(-${positionsTablet[index]}px)`
+        currentSlide = index
+      } else {
+        carousel.style.transform = `translateX(-${positionsMobile[index]}px)`
+        currentSlide = index
+      }
+
+}
+
+const selectDot = (index) => {
+    dots.forEach((dot) => {
+        dot.checked = false
+    })
+    dots[index].checked = true
+    selectSlide(index)
+}
+
+dots.forEach((dot) => {
+    dot.addEventListener("click", () => {
+        selectDot(dot.id)
+    })
+})
+
+setInterval(() => {
+    if (currentSlide >= 0 && currentSlide != 2) {
+        selectDot(currentSlide + 1)
+    } else{
+        selectDot(0)
+    }
+}, 5000)
+
+
+const burger = document.querySelector(".header__burger")
+const mobileMenu = document.querySelector(".mobile__menu")
+burger.addEventListener("click", () => {
+    burger.classList.toggle("open")
+    mobileMenu.classList.toggle("open")
+})
